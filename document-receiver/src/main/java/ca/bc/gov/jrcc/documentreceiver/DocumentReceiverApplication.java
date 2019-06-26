56af81ad09fe686b621e994c172d910014c18c21
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-06-26 09:15:07 -0700

    Adding spring base application created with spring initializr

---
+++ document-receiver/src/main/java/ca/bc/gov/jrcc/documentreceiver/DocumentReceiverApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.jrcc.documentreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentReceiverApplication.class, args);
+	}
+
+}
