eaa47499dfc437a4f7348c24502975f8d78be3fd
alexjoybc <alexandre.joyeux@gov.bc.ca>
2019-06-28 11:27:27 -0700

    adding app from initializr

---
+++ jrcc-document-receiver/src/main/java/ca/gov/bc/open/jrccdocumentreceiver/JrccDocumentReceiverApplication.java
@@ -0,0 +1,13 @@
+package ca.gov.bc.open.jrccdocumentreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrccDocumentReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrccDocumentReceiverApplication.class, args);
+	}
+
+}
